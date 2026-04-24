3ec593df87954955ea834e7c90cda3fdb126710a
indranil1983 <32717643+indranil1983@users.noreply.github.com>
2026-04-24 20:30:40 +0200

    Update main.tf

--- tofu/main.tf
+++ tofu/main.tf
@@ -6,37 +6,52 @@ terraform {
     }
   }
 }
+
 provider "libvirt" {
   uri = "qemu:///system"
 }
 
-# Base Ubuntu image (read-only)
+# ---------------------------------------------------
+# BASE IMAGE (download once, reused safely)
+# ---------------------------------------------------
 resource "libvirt_volume" "ubuntu_base" {
   name   = "ubuntu-base.qcow2"
   source = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
   format = "qcow2"
 }
 
-# Per-VM disks (IMPORTANT FIX)
+# ---------------------------------------------------
+# PER-VM DISKS (CRITICAL FIX: no shared disk)
+# ---------------------------------------------------
 resource "libvirt_volume" "disk" {
-  count  = 3
-  name   = "k8s-node-${count.index}.qcow2"
+  count = 3
+
+  name           = "k8s-node-${count.index}.qcow2"
   base_volume_id = libvirt_volume.ubuntu_base.id
-  format = "qcow2"
+  format         = "qcow2"
 }
 
-# Cloud-init
+# ---------------------------------------------------
+# CLOUD INIT
+# ---------------------------------------------------
 resource "libvirt_cloudinit_disk" "common" {
   name      = "cloudinit.iso"
   user_data = file("${path.module}/cloud_init.cfg")
 }
 
-# VMs
+# ---------------------------------------------------
+# VMS
+# ---------------------------------------------------
 resource "libvirt_domain" "vm" {
   count  = 3
   name   = "k8s-node-${count.index}"
+
   memory = 3072
   vcpu   = 2
+
+  cpu {
+    mode = "host-passthrough"
+  }
 
   disk {
     volume_id = libvirt_volume.disk[count.index].id
@@ -48,9 +63,16 @@ resource "libvirt_domain" "vm" {
 
   cloudinit = libvirt_cloudinit_disk.common.id
 
- console {
-  type        = "pty"
-  target_type = "serial"
-  target_port = 0
-}
+  console {
+    type        = "pty"
+    target_type = "serial"
+    target_port = 0
+  }
+
+  graphics {
+    type = "spice"
+  }
+
+  # Prevents name collisions on re-run
+  autostart = false
 }
