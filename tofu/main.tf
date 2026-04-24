29c4c428d574c32e8cbfe57fe76476a698d45c60
indranil1983 <32717643+indranil1983@users.noreply.github.com>
2026-04-24 20:12:51 +0200

    Update main.tf

--- tofu/main.tf
+++ tofu/main.tf
@@ -10,12 +10,28 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-resource "libvirt_volume" "ubuntu" {
-  name   = "ubuntu.qcow2"
+# Base Ubuntu image (read-only)
+resource "libvirt_volume" "ubuntu_base" {
+  name   = "ubuntu-base.qcow2"
   source = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
   format = "qcow2"
 }
 
+# Per-VM disks (IMPORTANT FIX)
+resource "libvirt_volume" "disk" {
+  count  = 3
+  name   = "k8s-node-${count.index}.qcow2"
+  base_volume_id = libvirt_volume.ubuntu_base.id
+  format = "qcow2"
+}
+
+# Cloud-init
+resource "libvirt_cloudinit_disk" "common" {
+  name      = "cloudinit.iso"
+  user_data = file("${path.module}/cloud_init.cfg")
+}
+
+# VMs
 resource "libvirt_domain" "vm" {
   count  = 3
   name   = "k8s-node-${count.index}"
@@ -23,7 +39,7 @@ resource "libvirt_domain" "vm" {
   vcpu   = 2
 
   disk {
-    volume_id = libvirt_volume.ubuntu.id
+    volume_id = libvirt_volume.disk[count.index].id
   }
 
   network_interface {
@@ -31,9 +47,8 @@ resource "libvirt_domain" "vm" {
   }
 
   cloudinit = libvirt_cloudinit_disk.common.id
-}
 
-resource "libvirt_cloudinit_disk" "common" {
-  name      = "commoninit.iso"
-  user_data = file("${path.module}/cloud_init.cfg")
+  console {
+    type = "pty"
+  }
 }
