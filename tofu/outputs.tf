33a63ed80bd93e56489aa89a38eb1484ccef5cb6
indranil1983 <32717643+indranil1983@users.noreply.github.com>
2026-04-24 20:13:54 +0200

    Update outputs.tf

--- tofu/outputs.tf
+++ tofu/outputs.tf
@@ -1,14 +1,3 @@
-output "master_ip" {
-  value = libvirt_domain.vm[0].network_interface[0].addresses[0]
-}
-
-output "worker_ips" {
-  value = [
-    for i in range(1, length(libvirt_domain.vm)) :
-    libvirt_domain.vm[i].network_interface[0].addresses[0]
-  ]
-}
-
-output "all_ips" {
-  value = libvirt_domain.vm[*].network_interface[0].addresses[0]
+output "ips" {
+  value = libvirt_domain.vm[*].network_interface[0].addresses
 }
