3802bf329a3a9fe2d7b02bc1250f7cd87c63d975
indranil1983 <32717643+indranil1983@users.noreply.github.com>
2026-04-24 18:57:09 +0200

    Update main.tf

--- tofu/main.tf
+++ tofu/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "~> 0.7.0"
+    }
+  }
+}
 provider "libvirt" {
   uri = "qemu:///system"
 }
