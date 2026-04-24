8bcd96c550f1be3a5eaf64d7fce7e035c9fb5602
indranil1983 <32717643+indranil1983@users.noreply.github.com>
2026-04-24 19:02:08 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
-
+sudo virsh pool-define-as default dir - - - - "/var/lib/libvirt/images"
+sudo virsh pool-build default
+sudo virsh pool-start default
+sudo virsh pool-autostart default
+virsh pool-list --all
 echo "Creating VMs..."
 cd tofu
 tofu init
