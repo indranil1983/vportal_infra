385d51e372413bf5c4f2050f74343be17e92c669
indranil1983 <32717643+indranil1983@users.noreply.github.com>
2026-04-24 20:07:42 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,7 +29,7 @@ echo "Creating VMs..."
 cd tofu
 tofu init
 sudo chmod -R 777 /var/lib/libvirt/images/ubuntu.qcow2
-sudo chmod -R /var/lib/libvirt/images
+sudo chmod -R 777 /var/lib/libvirt/images
 tofu apply -auto-approve
 
 echo "Waiting for VMs..."
