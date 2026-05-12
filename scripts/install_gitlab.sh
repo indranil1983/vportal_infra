1460932fa353de5b31f2aae96b276b04695ac040
Indranil Chandra <chandra.indranil@gmail.com>
2026-05-12 16:23:48 +0200

    Added Gitlab installer

---
+++ scripts/install_gitlab.sh
@@ -0,0 +1,11 @@
+sudo systemctl enable --now ssh
+sudo ufw allow 22/tcp
+sudo ufw allow 80/tcp
+sudo ufw allow 443/tcp
+sudo ufw enable
+sudo apt update
+sudo apt install -y curl
+
+curl --location "https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh" | sudo bash
+
+sudo GITLAB_ROOT_EMAIL="vplatform@gmail.com" GITLAB_ROOT_PASSWORD="Vplatform123" EXTERNAL_URL="https://gitlab.vplatform.local" apt install gitlab-ce
